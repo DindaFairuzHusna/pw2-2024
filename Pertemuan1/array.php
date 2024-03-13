31d0f4a86a63636c3a869f45803826377942e961
dinda fairuz husna <dfairuzhusna@gmail.com>
2024-03-13 11:16:47 +0700

    new commit

---
+++ Pertemuan1/array.php
@@ -0,0 +1,49 @@
+<?php
+
+    // $fruits = ["Mangga", "Apel", "Semangka", "Melon", "Manggis"];
+    
+    // foreach ($fruits as $fruit) {
+    //     echo $fruit;
+    //     echo '<br>';
+    // }
+
+
+
+
+
+    // $dinos = [
+    //     "dinoName" => "Tricera",
+    //     "dinoAge"  => 300,
+    //     "isMarried" => false
+    // ];
+
+
+    // foreach ($dinos as $dino) {
+    //     echo $dino;
+    //     echo '<br>';
+    // }
+
+    $dinos = [
+        [
+            "dinoName" => "Tricera",
+            "dinoAge" => 300
+        ],
+        [
+            "dinoName" => "Rex",
+            "dinoAge" => 500
+        ],
+        [
+            "dinoName" => "Ptera",
+            "dinoAge" => 250
+        ],
+    ];
+
+    foreach ($dinos as $dino) {
+        echo $dino["dinoName"] . " - " .$dino["dinoAge"] . " Tahun";
+        echo '<br>';
+    }
+
+
+
+
+?>
